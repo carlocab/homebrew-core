403d415092e271431169701a3c6a8972b41f51e8
danielnachun <daniel.nachun@gmail.com>
2022-10-04 00:06:54 -0700

    makepkg: depend on python@3.10 to build

--- Formula/makepkg.rb
+++ Formula/makepkg.rb
@@ -19,6 +19,7 @@ class Makepkg < Formula
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
+  depends_on "python@3.10" => :build
   depends_on "bash"
   depends_on "fakeroot"
   depends_on "gettext"
