0dc71de7440cae782dd2c0db8f0006b8ce4b7be4
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2023-04-27 22:07:41 +0800

    root: switch to `openssl@3`

--- Formula/root.rb
+++ Formula/root.rb
@@ -4,7 +4,7 @@ class Root < Formula
   url "https://root.cern.ch/download/root_v6.26.06.source.tar.gz"
   sha256 "b1f73c976a580a5c56c8c8a0152582a1dfc560b4dd80e1b7545237b65e6c89cb"
   license "LGPL-2.1-or-later"
-  revision 2
+  revision 3
   head "https://github.com/root-project/root.git", branch: "master"
 
   livecheck do
@@ -40,7 +40,7 @@ class Root < Formula
   depends_on "mysql-client"
   depends_on "numpy" # for tmva
   depends_on "openblas"
-  depends_on "openssl@1.1"
+  depends_on "openssl@3"
   depends_on "pcre"
   depends_on "python@3.10"
   depends_on "sqlite"
