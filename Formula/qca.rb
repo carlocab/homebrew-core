977107290d65d04031fbb5b9872a493ecc93271b
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2023-07-01 01:09:35 +0800

    qca: switch to Qt6

--- Formula/qca.rb
+++ Formula/qca.rb
@@ -4,6 +4,7 @@ class Qca < Formula
   url "https://download.kde.org/stable/qca/2.3.6/qca-2.3.6.tar.xz"
   sha256 "ee59d531d4b82fb1685f4d8d74c2caa0777f501800f7426eaa372109a4305249"
   license "LGPL-2.1-or-later"
+  revision 1
   head "https://invent.kde.org/libraries/qca.git", branch: "master"
 
   livecheck do
@@ -24,23 +25,24 @@ class Qca < Formula
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
   depends_on "botan"
+  depends_on "ca-certificates"
   depends_on "gnupg"
   depends_on "libgcrypt"
   depends_on "nss"
   depends_on "openssl@3"
   depends_on "pkcs11-helper"
-  depends_on "qt@5"
+  depends_on "qt"
 
   fails_with gcc: "5"
 
   def install
-    # Make sure we link with OpenSSL 3 and not OpenSSL 1.1.
-    openssl11 = Formula["openssl@1.1"]
-    ENV.remove "CMAKE_PREFIX_PATH", openssl11.opt_prefix
-    ENV.remove ["CMAKE_INCLUDE_PATH", "HOMEBREW_INCLUDE_PATHS"], openssl11.opt_include
-    ENV.remove ["CMAKE_LIBRARY_PATH", "HOMEBREW_LIBRARY_PATHS"], openssl11.opt_lib
-
-    args = %W[-DBUILD_TESTS=OFF -DQCA_PLUGINS_INSTALL_DIR=#{lib}/qt5/plugins]
+    ENV["QC_CERTSTORE_PATH"] = Formula["ca-certificates"].pkgetc/"cert.pem"
+    args = %W[
+      -DBUILD_TESTS=OFF
+      -DBUILD_WITH_QT6=ON
+      -DQCA_PLUGINS_INSTALL_DIR=#{share}/qt/plugins
+    ]
+    args << "-DQCA_LIBRARY_INSTALL_DIR=#{frameworks}" if OS.mac?
 
     # Disable some plugins. qca-ossl, qca-cyrus-sasl, qca-logger,
     # qca-softstore are always built.
@@ -48,18 +50,19 @@ class Qca < Formula
       args << "-DWITH_#{plugin}_PLUGIN=ON"
     end
 
-    # ensure opt_lib for framework install name and linking (can't be done via CMake configure)
-    inreplace "src/CMakeLists.txt",
-              /^(\s+)(INSTALL_NAME_DIR )("\$\{QCA_LIBRARY_INSTALL_DIR\}")$/,
-             "\\1\\2\"#{opt_lib}\""
-
     system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
+
+    return unless OS.mac?
+
+    lib.install_symlink (frameworks/"qca-qt6.framework/qca-qt6") => shared_library("libqca-qt6")
+    (include/"Qca-qt6/QtCrypto").install_symlink (frameworks/"qca-qt6.framework/Headers").children
   end
 
   test do
-    system bin/"qcatool-qt5", "--noprompt", "--newpass=",
+    ENV["LC_ALL"] = "en_US.UTF-8"
+    system bin/"qcatool-qt6", "--noprompt", "--newpass=",
                               "key", "make", "rsa", "2048", "test.key"
   end
 end
